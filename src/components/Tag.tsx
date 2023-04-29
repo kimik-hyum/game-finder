fb41cb3c1cc632b4ec5937dabfe7ece41a58aeb2
김현준 <hjk@interiorteacher.com>
2023-04-29 21:56:00 +0900

    타입 수정

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,6 +1,6 @@
 import { FCT } from "@/type/common";
 
-const Tag: FCT<"span"> = ({ children }) => {
+const Tag: FCT = ({ children }) => {
   return (
     <span className="inline-flex py-0.5 px-1 text-gray-950 bg-gray-300 mr-1 rounded text-sm">
       {children}
